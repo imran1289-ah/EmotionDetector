1e08d282682e41707f55aa43c63999e5228440c3
imran1289-ah <77070862+imran1289-ah@users.noreply.github.com>
2024-03-18 14:26:52 -0400

    splitting dataset using pytorch/creating dataloaders/intializing cnn

--- model-train.py
+++ model-train.py
@@ -5,14 +5,29 @@ import torchvision.transforms as transforms
 import torchvision.datasets
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from torchvision.datasets import ImageFolder
+from cnn_model import CNN
 
+#Initializing dataset
+image_path = "dataset/transformed_dataset"
+dataset = ImageFolder(root=image_path)
 
-df = pd.read_csv("dataset/emotiondata.csv")
+#Splitting dataset to train/test/validation
+train_set = int(0.7 *len(dataset))
+validation_set = int(0.15*len(dataset))
+test_set = int(0.15*len(dataset))
 
-X = df["ImagePath"].astype(str)
-y = df["Label"].astype(str)
+train_set, validation_set, test_set = torch.utils.data.random_split(dataset, [train_set,validation_set, test_set])
 
-#Splitting dataset to 70/15/15
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-X_valid, X_test, y_valid, y_test = train_test_split(X_test, y_test, test_size=0.5, random_state=42)
+#Set data loaders
+train_loader = DataLoader(train_set, batch_size=32, shuffle=False)
+test_loader = DataLoader(test_set, batch_size=32, shuffle=False)
+validation_loader = DataLoader(validation_set, batch_size=32, shuffle=False)
 
+#Initalizing the custom model
+model = CNN()
+
+#Defining loss and optimizer function
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
